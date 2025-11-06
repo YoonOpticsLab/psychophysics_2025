e3601fd23f9e10b76b93fc9aa73f18dd2e7401d1
Dan Coates <dcoates72@gmail.com>
2025-11-06 17:54:40 -0600

    Renamed for sensibility.

--- run_experiment.m
+++ run_experiment.m
@@ -1,10 +1,8 @@
 one_pixel_cm=monitor_horiz_size_cm/monitor_horiz_num_pixels
 arcmin_per_pixel = atan( one_pixel_cm/distance_cm ) / pi * 180 * 60
 
-
-
 if strcmp(which_experiment,'blur1')
-    blur1_run;
+    code_blur1;
 elseif strcmp(which_experiment,'embedded_num')
-    embedded_number_run;
+    code_embedded_number;
 end
